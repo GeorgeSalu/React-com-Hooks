90ea1dba163a36c4cec8fbadf66d691cc4d18bb4
George Salu <george.salu10@gmail.com>
2020-03-31 19:18:47 -0300

    importando bootstrap

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Calculadora from './calculadora';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
