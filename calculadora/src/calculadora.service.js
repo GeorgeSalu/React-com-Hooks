112a03cd711149ddc0cfa42659c78c0a3cd332a4
George Salu <george.salu10@gmail.com>
2020-04-01 15:52:30 -0300

    implementando o servico principal da calculadora 3/5

--- calculadora/src/calculadora.service.js
+++ calculadora/src/calculadora.service.js
@@ -1,6 +1,7 @@
 function CalculadoraService() {
 
   const SOMA = '+'
+  const SUBTRACAO = '-'
 
   function calcular(numero1, numero2, operacao) {
     let resultado;
@@ -9,6 +10,9 @@ function CalculadoraService() {
       case SOMA:
         resultado = numero1 + numero2
         break;
+      case SUBTRACAO:
+        resultado = numero1 - numero2
+        break;
     }
     
     return resultado;
@@ -16,7 +20,7 @@ function CalculadoraService() {
 
 
   return [
-    calcular
+    calcular, SOMA, SUBTRACAO
   ];
 }
 
--- calculadora/src/calculadora.service.test.js
+++ calculadora/src/calculadora.service.test.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom'
 import CalculadoraService from './calculadora.service'
 
 describe('teste do CalculadoraService', () => {
-  const [calcular] = CalculadoraService();
+  const [calcular, SOMA, SUBTRACAO] = CalculadoraService();
 
   it('deve garantir que 1 + 4 = 5', () => {
-    let soma = calcular(1,4, '+');
+    let soma = calcular(1,4, SOMA);
     expect(soma).toEqual(5);
+  })
+
+  it('deve garantir que 1 - 4 = -3', () => {
+    let subtracao = calcular(1,4, SUBTRACAO);
+    expect(subtracao).toEqual(-3);
   })
 })
