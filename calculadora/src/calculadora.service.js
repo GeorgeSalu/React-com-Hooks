7743e0800915d3ce2bb343681aacc570c3881b89
George Salu <george.salu10@gmail.com>
2020-04-01 15:48:03 -0300

    implementando o servico principal da calculadora 3/5

--- calculadora/src/calculadora.service.js
+++ calculadora/src/calculadora.service.js
@@ -1,9 +1,23 @@
 function CalculadoraService() {
 
+  const SOMA = '+'
+
   function calcular(numero1, numero2, operacao) {
-    return 0
+    let resultado;
+
+    switch(operacao) {
+      case SOMA:
+        resultado = numero1 + numero2
+        break;
+    }
+    
+    return resultado;
   }
 
+
+  return [
+    calcular
+  ];
 }
 
 export default CalculadoraService
