10846886027ceb133b5b9b86eba6326c85116280
George Salu <george.salu10@gmail.com>
2020-04-01 22:44:23 -0300

    criando os teste unitarios 1/4

--- calculadora/src/calculadora.test.js
+++ calculadora/src/calculadora.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import Calculadora from './calculadora';
 
-test('renders learn react link', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Calculadora />, div)
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('Calculadora', () => {
+
+  it('deve renderizar o componente sem erros', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Calculadora />, div)
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+})
