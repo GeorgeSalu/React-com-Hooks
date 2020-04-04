f08c4a235d2b1933658841ff119a2c69173eef75
George Salu <george.salu10@gmail.com>
2020-04-04 13:37:46 -0300

    criando o componente principal

--- conversor-de-moedas/src/index.js
+++ conversor-de-moedas/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ConversorMoedas from './conversor-moedas';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
