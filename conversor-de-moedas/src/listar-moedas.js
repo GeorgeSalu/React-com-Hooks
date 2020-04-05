4bb5fffe5457a4541441737c457d6b466483f54b
George Salu <george.salu10@gmail.com>
2020-04-05 12:28:22 -0300

    criando o componente de listagem de moedas - 3/4

--- conversor-de-moedas/src/listar-moedas.js
+++ conversor-de-moedas/src/listar-moedas.js
@@ -37,9 +37,20 @@ function ListarMoedas() {
       { "sigla": "ZAR", "descricao": "Rand África do Sul" }
     ]; 
 
-    
+    function compare(moeda1, moeda2) {
+      if(moeda1.descricao < moeda2.descricao) {
+        return -1
+      } else if (moeda1.descricao > moeda2.descricao) {
+        return 1;
+      }
+      return 0;
+    }
 
-    return ();
+    return MOEDAS.sort(compare).map(moeda =>
+      <option value={moeda.sigla} key={moeda.sigla}>
+        {moeda.descricao}
+      </option>
+    );
 }
 
 export default ListarMoedas
