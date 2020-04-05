bf891bf2a9f01e45905c14c23fce431f5942ef84
George Salu <george.salu10@gmail.com>
2020-04-05 11:27:56 -0300

    criando o formulario html 2/4

--- conversor-de-moedas/src/conversor-moedas.js
+++ conversor-de-moedas/src/conversor-moedas.js
@@ -11,7 +11,28 @@ function ConversorMoedas() {
     <div>
       <h1>ConversorMoedas</h1>
       <Jumbotron>
-        
+        <Form>
+          <Form.Row>
+            <Col sm="3">
+              <Form.Control 
+                placeholder="0" 
+                value={1} 
+                required/> 
+            </Col>
+            <Col sm="3">
+              
+            </Col>
+            <Col sm="1">
+              
+            </Col>
+            <Col sm="3">
+              
+            </Col>
+            <Col sm="2">
+
+            </Col>
+          </Form.Row>
+        </Form>
       </Jumbotron>
     </div>
   );
