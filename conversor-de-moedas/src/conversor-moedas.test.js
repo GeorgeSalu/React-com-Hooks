523155ca444e0cb32ee0e61e6f48e32dc8aed2e0
George Salu <george.salu10@gmail.com>
2020-04-08 15:09:12 -0300

    criando os teste unitarios 2/4

--- conversor-de-moedas/src/conversor-moedas.test.js
+++ conversor-de-moedas/src/conversor-moedas.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ConversorMoedas from './conversor-moedas';
+import {render, fireEvent} from '@testing-library/react'
+import axiosMock from 'axios';
 
-it('deve retornar o componente sem erros', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ConversorMoedas />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Teste do componente de conversao de moedas', => {
+
+	it('deve retornar o componente sem erros', () => {
+	  const div = document.createElement('div');
+	  ReactDOM.render(<ConversorMoedas />, div);
+	  ReactDOM.unmountComponentAtNode(div);
+	})
+
+  it('deve simular uma conversao de moedas', => {
+    const { findByTestId, getByTestId } = render(<ConversorMoedas />);
+    axiosMock.get.mockResolvedValueOnce({
+      data: {success: true, rates: {BRL: 4.564292, USD: 1.101049}}
+    });
+  })
+
 })
+
