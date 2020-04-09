e0ec7240fae8cba76648bbef04ccafc100ed79cd
George Salu <george.salu10@gmail.com>
2020-04-09 11:21:09 -0300

    criando o formulario de cadastro de tarefas 1/2

--- gerenciador-de-tarefas/src/cadastrar/cadastrar-tarefa.js
+++ gerenciador-de-tarefas/src/cadastrar/cadastrar-tarefa.js
@@ -3,7 +3,28 @@ import { Button, Form, Jumbotron, Modal } from 'react-bootstrap'
 import { navigate, A } from 'hookrouter'
 
 function CadastrarTarefa() {
-	return <h1>Cadastrar Tarefa</h1>
+	return (
+		<div>
+      <h3 className="text-center">Cadastrar</h3>
+      <Jumbotron>
+        <Form>
+          <Form.Group>
+            <Form.Label>Tarefa</Form.Label>
+            <Form.Control 
+              type="text"
+              placeholder="Digite a tarefa"
+              minLength="5"
+              maxLength="100"
+              required />
+
+            <Form.Control.Feedback type="invalid">
+              A Tarefa deve conter ao menos 5 caracteres
+            </Form.Control.Feedback> 
+          </Form.Group>
+        </Form>
+      </Jumbotron>  
+    </div>
+	)
 }
 
 export default CadastrarTarefa
