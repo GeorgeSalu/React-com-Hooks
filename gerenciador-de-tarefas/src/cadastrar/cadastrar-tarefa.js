d78f1ca242e89e723d6adaf9a6da933a3b3d9ceb
George Salu <george.salu10@gmail.com>
2020-04-09 12:13:43 -0300

    implementando a acao de cadastro de tarefas 1/2

--- gerenciador-de-tarefas/src/cadastrar/cadastrar-tarefa.js
+++ gerenciador-de-tarefas/src/cadastrar/cadastrar-tarefa.js
@@ -3,11 +3,27 @@ import { Button, Form, Jumbotron, Modal } from 'react-bootstrap'
 import { navigate, A } from 'hookrouter'
 
 function CadastrarTarefa() {
+
+  const [tarefa, setTarefa] = useState('');
+  const [formValidado, setFormValidado] = useState(false);
+  const [exibirModal, setExibirModal] = useState(false);
+
+  function cadastrar(event) {
+
+  }
+
+  function handleTxtTarefa(event) {
+    setTarefa(event.target.value)
+  }
+
 	return (
 		<div>
       <h3 className="text-center">Cadastrar</h3>
       <Jumbotron>
-        <Form>
+        <Form 
+          validated={formValidado}
+          noValidate
+          onSubmit={cadastrar}>
           <Form.Group>
             <Form.Label>Tarefa</Form.Label>
             <Form.Control 
@@ -15,7 +31,9 @@ function CadastrarTarefa() {
               placeholder="Digite a tarefa"
               minLength="5"
               maxLength="100"
-              required />
+              required
+              value={tarefa}
+              onChange={handleTxtTarefa} />
 
             <Form.Control.Feedback type="invalid">
               A Tarefa deve conter ao menos 5 caracteres
