419f594a8fa7edf65c1f16f60563c1ecdc590e63
George Salu <george.salu10@gmail.com>
2020-04-12 15:00:38 -0300

    criando os teste unitarios do componente de itens das tarefas 1/3

---
+++ gerenciador-de-tarefas/src/listar/itens-lista-tarefas.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+import ItensListaTarefas from './itens-lista-tarefas';
+import Tarefa from '../models/tarefa.model';
+import {render, fireEvent} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('Teste do componente que exibe um item da listagem de tarefas', () => {
+	
+})
