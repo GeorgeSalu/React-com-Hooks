f560976c2f49abc39d8ecd0ec2aad9e9d1510191
George Salu <george.salu10@gmail.com>
2020-04-12 14:09:58 -0300

    criando o componente de itens de listagem de tarefas 2/2

--- gerenciador-de-tarefas/src/listar/itens-lista-tarefas.js
+++ gerenciador-de-tarefas/src/listar/itens-lista-tarefas.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 function ItemsListaTarefas(props) {
   return ();
+}
+
+ItemsListaTarefas.propTypes = {
+  tarefas: PropTypes.array.isRequired,
+  recarregarTarefas: PropTypes.func.isRequired
 }
 
 export default ItemsListaTarefas;
