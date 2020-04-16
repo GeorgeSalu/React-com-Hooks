8393c946c487e5cd4be92e39a804c31e4153db4b
George Salu <george.salu10@gmail.com>
2020-04-16 13:29:11 -0300

    criando o botao de conclusao de tarefas

--- gerenciador-de-tarefas/src/listar/concluir-tarefa.js
+++ gerenciador-de-tarefas/src/listar/concluir-tarefa.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Modal, Button} from 'react-bootstrap'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faClipboardCheck } from '@fortawesome/free-solid-svg-icons';
 
 function ConcluirTarefa(props) {
-  return ();
+
+  function handleAbrirModal(event) {
+    event.preventDefault();
+    
+  }
+
+  return (
+    <span className={props.className}>
+      <Button className="btn-sm" onClick={handleAbrirModal}
+        data-testid="btn-abrir-modal">
+        <FontAwesomeIcon icon={faClipboardCheck} />
+      </Button>
+    </span>
+  );
 }
 
 ConcluirTarefa.propTypes = {
