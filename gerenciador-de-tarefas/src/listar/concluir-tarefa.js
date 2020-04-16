d0db1d124843e0156ea376c571bb9bf988a164c3
George Salu <george.salu10@gmail.com>
2020-04-16 13:44:20 -0300

    criando o html da modal de conclusao de tarefas

--- gerenciador-de-tarefas/src/listar/concluir-tarefa.js
+++ gerenciador-de-tarefas/src/listar/concluir-tarefa.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import {Modal, Button} from 'react-bootstrap'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
@@ -6,9 +6,16 @@ import { faClipboardCheck } from '@fortawesome/free-solid-svg-icons';
 
 function ConcluirTarefa(props) {
 
+  const [exibirModal, setExibirModal] = useState(false);
+
+
   function handleAbrirModal(event) {
     event.preventDefault();
-    
+    setExibirModal(true);
+  }
+
+  function handleFecharModal() {
+    setExibirModal(false);
   }
 
   return (
@@ -17,6 +24,27 @@ function ConcluirTarefa(props) {
         data-testid="btn-abrir-modal">
         <FontAwesomeIcon icon={faClipboardCheck} />
       </Button>
+      <Modal show={exibirModal} onHide={handleFecharModal}
+        data-testid="modal">
+        <Modal.Header closeButton>
+          <Modal.Title>Concluir tarefa</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Deseja realmente concluir a seguinte tarefa ?
+          <br />
+          <strong>{props.tafera.nome}</strong>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleConcluirTarefa}
+            data-testid="btn-concluir" >
+            Sim
+          </Button>
+          <Button variant="light" onClick={handleFecharModal} 
+            data-testid="btn-fechar-modal">
+            Não
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </span>
   );
 }
