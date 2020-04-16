b40374fb1e298e9c21414fa3fec798a9e8313a93
George Salu <george.salu10@gmail.com>
2020-04-16 15:37:42 -0300

    criando o html para remover uma tarefa

--- gerenciador-de-tarefas/src/listar/remover-tarefa.js
+++ gerenciador-de-tarefas/src/listar/remover-tarefa.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Modal, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 
 function RemoverTarefa(props) {
-
+  return (
+    <span>
+      <Button variant="danger" className="btn-sm"
+        onClick={handleAbrirModal} 
+        data-testid="btn-abrir-modal">
+        <FontAwesomeIcon icon={faTrashAlt} />
+      </Button>
+    </span>
+  );
 }
 
 RemoverTarefa.propTypes = {
