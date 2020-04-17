7e9e6bc0adc6901f5f593a62f9e2ec1bbe47f560
George Salu <george.salu10@gmail.com>
2020-04-17 15:33:26 -0300

    criando os teste unitarios para filtragem de tarefas 2/3

--- gerenciador-de-tarefas/src/listar/listar-tarefas.test.js
+++ gerenciador-de-tarefas/src/listar/listar-tarefas.test.js
@@ -11,11 +11,30 @@ describe('Teste do componente de listagem de tarefas', () => {
   const nomeSegundaTarefa = 'Segunda Tarefa'
   const nomeTerceiraTarefa = 'Terceira Tarefa'
 
+  beforeEach(() => {
+    localStorage['tarefas'] = JSON.stringify([
+      new Tarefa(1, nomePrimeirTarefa, false),
+      new Tarefa(2, nomeSegundaTarefa, false),
+      new Tarefa(3, nomeTerceiraTarefa, false)
+    ])
+  })
+
+  afterEach(() => {
+    delete localStorage['tarefas'];
+  })
 
 	it('deve renderizar o componente sem erros', () => {
     const div = document.createElement('div');
     ReactDOM.render(<ListarTarefas />, div);
     ReactDOM.unmountComponentAtNode(div);
+  })
+
+  it('deve exibir uma tabela contendo 3 tarefas',() => {
+    const {getByTestId} = render(<ListarTarefas />);
+    const tabela = getByTestId('tabela');
+    expect(tabela).toHaveTextContent(nomePrimeirTarefa)
+    expect(tabela).toHaveTextContent(nomeSegundaTarefa)
+    expect(tabela).toHaveTextContent(nomeTerceiraTarefa)
   })
 
 })
