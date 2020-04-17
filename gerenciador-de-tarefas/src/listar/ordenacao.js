11bcb6d8d7bded055160031599f1f1075a0402f2
George Salu <george.salu10@gmail.com>
2020-04-17 11:52:21 -0300

    adicionando a ordenacao na tela de listagem de tarefas 2/3

--- gerenciador-de-tarefas/src/listar/ordenacao.js
+++ gerenciador-de-tarefas/src/listar/ordenacao.js
@@ -9,12 +9,28 @@ function Ordenacao(props) {
     return (props.ordenarAsc || props.ordenarDesc ) ? 'hidden' : '';
   }
 
+  function handleAsc() {
+    return props.ordenarAsc ? '' : 'hidden';
+  }
+
+  function handleDesc() {
+    return props.ordenarDesc ? '' : 'hidden';
+  }
+
   return (
     <span>
       <FontAwesomeIcon
         icon={faSort}
         className={handleAscDesc()}
         data-testid="faSort" />
+      <FontAwesomeIcon
+        icon={faSortUp}
+        className={handleAsc()}
+        data-testid="faSortUp" />
+      <FontAwesomeIcon
+        icon={faSortDown}
+        className={handleDesc()}
+        data-testid="faSortDown" />
     </span>
   );
 }
