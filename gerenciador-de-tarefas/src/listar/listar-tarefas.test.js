333f9ef5234334fdf8b5970dd345d42afed6ce06
George Salu <george.salu10@gmail.com>
2020-04-17 15:27:01 -0300

    criando os teste unitarios para filtragem de tarefas 1/3

--- gerenciador-de-tarefas/src/listar/listar-tarefas.test.js
+++ gerenciador-de-tarefas/src/listar/listar-tarefas.test.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ListarTarefas from './listar-tarefas'
+import Tarefa from '../models/tarefa.model'
+import {render, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 
 describe('Teste do componente de listagem de tarefas', () => {
+
+  const nomePrimeirTarefa = 'Primeira Tarefa'
+  const nomeSegundaTarefa = 'Segunda Tarefa'
+  const nomeTerceiraTarefa = 'Terceira Tarefa'
+
+
 	it('deve renderizar o componente sem erros', () => {
     const div = document.createElement('div');
     ReactDOM.render(<ListarTarefas />, div);
     ReactDOM.unmountComponentAtNode(div);
   })
+
 })
