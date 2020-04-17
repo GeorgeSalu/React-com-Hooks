b206781a5a0b0895b4763c29ea9c83eb018f62a5
George Salu <george.salu10@gmail.com>
2020-04-17 11:16:51 -0300

    adicionando a ordenacao na tela de listagem de tarefas 1/3

---
+++ gerenciador-de-tarefas/src/listar/ordenacao.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSort, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons'
+import PropTypes from 'prop-types';
+
+function Ordenacao(props) {
+
+}
+
+Ordenacao.propTypes = {
+  ordenarAsc: PropTypes.bool.isRequired,
+  ordenarDesc: PropTypes.bool.isRequired
+}
+
+export default Ordenacao;
