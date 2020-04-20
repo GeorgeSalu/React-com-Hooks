a44fdf4da47a089893b1e0ccf45e346e0c0f0165
George Salu <george.salu10@gmail.com>
2020-04-20 15:50:21 -0300

    criando os teste unitarios da atualizacao de tarefas 1/3

--- gerenciador-de-tarefas/src/atualizar/atualizar-tarefa.test.js
+++ gerenciador-de-tarefas/src/atualizar/atualizar-tarefa.test.js
@@ -16,7 +16,7 @@ describe('Teste do componente de atualizacao de tarefas', () => {
 
 	it('deve renderizar o componente sem erros', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<AtualizarTarefa id={1} />, div);
+    ReactDOM.render(<AtualizarTarefa id={tarefaId} />, div);
     ReactDOM.unmountComponentAtNode(div)
   })
 
