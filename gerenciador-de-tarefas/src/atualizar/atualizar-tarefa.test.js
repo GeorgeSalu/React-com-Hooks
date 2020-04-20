2b0576af3156b4f7acd049e937d8847238c6b606
George Salu <george.salu10@gmail.com>
2020-04-20 15:47:06 -0300

    criando os teste unitarios da atualizacao de tarefas 1/3

--- gerenciador-de-tarefas/src/atualizar/atualizar-tarefa.test.js
+++ gerenciador-de-tarefas/src/atualizar/atualizar-tarefa.test.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import AtualizarTarefa from './atualizar-tarefa'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AtualizarTarefa from './atualizar-tarefa';
+import Tarefa from '../models/tarefa.model';
+import {render, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 
 describe('Teste do componente de atualizacao de tarefas', () => {
+
+  const tarefaId = 1;
+  const tarefa = new Tarefa(tarefaId, 'Nova Tarefa', false);
+
+  beforeEach(() => {
+    localStorage['tarefas'] = JSON.stringify([tarefa])
+  })
+
 	it('deve renderizar o componente sem erros', () => {
     const div = document.createElement('div');
     ReactDOM.render(<AtualizarTarefa id={1} />, div);
     ReactDOM.unmountComponentAtNode(div)
   })
+
+
+
 })
