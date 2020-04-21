4c699a201dc0b3fa53f898647f96bb2cb8864f43
George Salu <george.salu10@gmail.com>
2020-04-21 20:21:44 -0300

    definindo a listagem de tarefas

--- servidor-api/index.js
+++ servidor-api/index.js
@@ -8,4 +8,9 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
+//listar todas tarefas - get
+app.get('/gerenciador-tarefas', (req, res) => {
+  res.status(501).json({ erro: 'Nao implementado' })
+});
+
 app.listen(port, () => console.log(`servidor inicializado na porta ${port}`));
