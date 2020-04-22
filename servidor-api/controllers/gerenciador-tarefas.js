21852679ef7da2c1c9a595402c52ece9ce2f4f81
George Salu <george.salu10@gmail.com>
2020-04-22 11:49:15 -0300

    implementando a listagem de tarefas por id 1/2

---
+++ servidor-api/controllers/gerenciador-tarefas.js
@@ -0,0 +1,8 @@
+const uuidv4 = require('uuid/4');
+
+let tarefas = [
+  {id: '1', nome: 'aprender react', concluida: true},
+  {id: '2', nome: 'aprender javascript', concluida: false},
+  {id: '3', nome: 'aprender java', concluida: false},
+  {id: '4', nome: 'aprender react native', concluida: false},
+]
