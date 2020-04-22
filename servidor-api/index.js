9c76e4a8eb52b2c347dd55755b607841c8af1524
George Salu <george.salu10@gmail.com>
2020-04-22 11:13:16 -0300

    definindo a listagem de tarefas por id

--- servidor-api/index.js
+++ servidor-api/index.js
@@ -8,9 +8,14 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
+function naoImplementado(req,res) {
+  res.status(501).json({ erro: 'Nao implementado' });
+}
+
 //listar todas tarefas - get
-app.get('/gerenciador-tarefas', (req, res) => {
-  res.status(501).json({ erro: 'Nao implementado' })
-});
+app.get('/gerenciador-tarefas', naoImplementado);
+
+//listar uma tarefa por id
+app.get('/gerenciador-tarefas/:id', naoImplementado);
 
 app.listen(port, () => console.log(`servidor inicializado na porta ${port}`));
