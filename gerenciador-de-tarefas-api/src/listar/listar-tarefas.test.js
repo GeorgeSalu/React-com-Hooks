9f374df6a574e2533832ddddb74c325683d47222
George Salu <george.salu10@gmail.com>
2020-04-28 12:25:35 -0300

    criando os teste unitarios de listagem de tarefas 3/3

--- gerenciador-de-tarefas-api/src/listar/listar-tarefas.test.js
+++ gerenciador-de-tarefas-api/src/listar/listar-tarefas.test.js
@@ -12,16 +12,18 @@ describe('Teste do componente de listagem de tarefas', () => {
   const nomeSegundaTarefa = 'Segunda Tarefa'
   const nomeTerceiraTarefa = 'Terceira Tarefa'
 
+  const listaTarefas = {
+    totalItens: 3,
+    tarefas: [
+      new Tarefa(1, nomePrimeiraTarefa, false),
+      new Tarefa(2, nomeSegundaTarefa, false),
+      new Tarefa(3, nomeTerceiraTarefa, false),
+    ],
+    pagina: 1
+  }
+
   it('deve exibir uma tabela contendo 3 tarefas', async () => {
-    axiosMock.get.mockResolvedValueOnce({data: {
-      totalItens: 3,
-      tarefas: [
-        new Tarefa(1, nomePrimeiraTarefa, false),
-        new Tarefa(2, nomeSegundaTarefa, false),
-        new Tarefa(3, nomeTerceiraTarefa, false),
-      ],
-      pagina: 1
-    }})
+    axiosMock.get.mockResolvedValueOnce({data: listaTarefas})
 
     const {findByTestId} = render(<ListarTarefas />);
     const tabela = await findByTestId('tabela');
@@ -30,10 +32,20 @@ describe('Teste do componente de listagem de tarefas', () => {
     expect(tabela).toHaveTextContent(nomeTerceiraTarefa)
   })
 
-  it.skip('deve filtrar os dados da tabela de tarefas',() => {
-    const {getByTestId} = render(<ListarTarefas />);
-    fireEvent.change(getByTestId('txt-tarefa'), {target: {value: nomePrimeiraTarefa}})
-    const tabela = getByTestId('tabela')
+  it('deve filtrar os dados da tabela de tarefas',async () => {
+
+    axiosMock.get.mockResolvedValueOnce({data: listaTarefas});
+    axiosMock.get.mockResolvedValueOnce({data: {
+      totalItens: 1,
+      tarefas: [
+        new Tarefa(1, nomePrimeiraTarefa, false)
+      ],
+      pagina: 1
+    } });
+
+    const {findByTestId} = render(<ListarTarefas />);
+    fireEvent.change( await findByTestId('txt-tarefa'), {target: {value: nomePrimeiraTarefa}})
+    const tabela = await findByTestId('tabela')
     expect(tabela).toHaveTextContent(nomePrimeiraTarefa);
     expect(tabela).not.toHaveTextContent(nomeSegundaTarefa);
     expect(tabela).not.toHaveTextContent(nomeTerceiraTarefa);
