5e375725e3f4edee1c58214b68222e51f425482a
George Salu <george.salu10@gmail.com>
2020-04-28 21:42:55 -0300

    criando os teste unitarios de atualizacao de tarefas

--- gerenciador-de-tarefas-api/src/atualizar/atualizar-tarefa.test.js
+++ gerenciador-de-tarefas-api/src/atualizar/atualizar-tarefa.test.js
@@ -4,37 +4,18 @@ import AtualizarTarefa from './atualizar-tarefa';
 import Tarefa from '../models/tarefa.model';
 import {render, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
+import axiosMock from 'axios'
 
-describe.skip('Teste do componente de atualizacao de tarefas', () => {
+describe('Teste do componente de atualizacao de tarefas',  () => {
 
   const tarefaId = 1;
-  const tarefa = new Tarefa(tarefaId, 'Nova Tarefa', false);
 
-  beforeEach(() => {
-    localStorage['tarefas'] = JSON.stringify([tarefa])
-  })
-
-	it('deve renderizar o componente sem erros', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<AtualizarTarefa id={tarefaId} />, div);
-    ReactDOM.unmountComponentAtNode(div)
-  })
-
-  it('deve exibir a modal de sucesso ao atualizar uma tarefa',() => {
-    const {getByTestId} = render(<AtualizarTarefa id={tarefaId} />);
-
-    fireEvent.click(getByTestId('btn-atualizar'));
+  it('deve atualizar uma tarefa', async () => {
+    axiosMock.get.mockResolvedValueOne({ data: {nome: 'Estudar React'} })
+    const {findByTestId} = render(<AtualizarTarefa id={tarefaId} />)
+    fireEvent.click( await findByTestId('btn-atualizar'));
+    const modal = await findByTestId('modal');
     expect(getByTestId('modal')).toHaveTextContent('Sucesso');
-  })
-
-  it('deve atualizar uma tarefa',() => {
-    const nomeTarefaAtualizada = 'Tarefa atualizada';
-    const {getByTestId} = render(<AtualizarTarefa id={tarefaId} />)
-
-    fireEvent.change(getByTestId('txt-tarefa'), {target: {value: nomeTarefaAtualizada}});
-    fireEvent.click(getByTestId('btn-atualizar'));
-    const tarefasDb = JSON.parse(localStorage('tarefas'));
-    expect(tarefasDb[0].nome).toBe(nomeTarefaAtualizada);
   })
 
 })
