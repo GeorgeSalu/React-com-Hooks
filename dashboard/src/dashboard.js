e5587005a709f8491adcda4590aed8a081c77599
George Salu <george.salu10@gmail.com>
2020-05-02 13:56:06 -0300

    criando o grafico pieChart

--- dashboard/src/dashboard.js
+++ dashboard/src/dashboard.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './dashboard.css';
 import Chart from 'react-google-charts';
 
 function Dashboard() {
+
+  const TITULO = 'Quantidae de cadastros primeiro semestre';
+
+  const [dados, setDados] = useState([
+    ['Mês','Quantidade'],
+    ['Janeiro',33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27]
+  ]);
+
   return (
-    <h1>Dashboard</h1>
+    <div>
+      <h1>Dashboard</h1>
+      <Chart
+        width={'400px'}
+        height={'300px'}
+        chartType={'PieChart'}
+        data={dados}
+        options={{
+          title: TITULO,
+        }} />
+    </div>
+
+
   );
 }
 
