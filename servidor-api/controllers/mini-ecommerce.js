05527034927f2cd72a34f217e264d8e1273e3fac
George Salu <george.salu10@gmail.com>
2020-05-02 23:14:21 -0300

    implementando a listagem de cidades por estado na api 1/2

--- servidor-api/controllers/mini-ecommerce.js
+++ servidor-api/controllers/mini-ecommerce.js
@@ -1,6 +1,13 @@
+const cidadesEstados = require('../cidades-estados.json');
+
 function finalizarCompra(req, res) {
   console.log(req.body);
   res.send('ok')
+}
+
+function obterCidadesPorEstado(req, res) {
+  const siglaEstado = req.params['siglaEstado'].toUpperCase();
+  const dadosEstado = cidadesEstados.estados.filter(estado => estado.sigla === sigla.estado);
 }
 
 module.exports = {
