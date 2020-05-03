2c160af1c12003c2a5557ad4c64b9bafd147bf7b
George Salu <george.salu10@gmail.com>
2020-05-02 22:04:18 -0300

    implementando a finalização da compra na api 1/2

--- servidor-api/index.js
+++ servidor-api/index.js
@@ -10,7 +10,9 @@ const {
   removerTarefa,
   concluirTarefa
 } = require('./controllers/gerenciador-tarefas.js');
-
+const {
+  finalizarCompra
+} = require('./controllers/mini-ecommerce.js')
 
 const app = express();
 const port = 3001;
@@ -30,5 +32,8 @@ app.put('/gerenciador-tarefas/:id', atualizarTarefa);
 app.delete('/gerenciador-tarefas/:id', removerTarefa);
 // concluir uma tarefa - put
 app.put('/gerenciador-tarefas/:id/concluir', concluirTarefa);
+
+//mini-ecommerce
+app.post('/mini-ecommerce/checkout/finalizar-compra', finalizarCompra);
 
 app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
