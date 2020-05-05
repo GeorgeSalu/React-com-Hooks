b00ec1ae9bfaf53293aa8139c4bdac7b52c090a9
George Salu <george.salu10@gmail.com>
2020-05-04 22:03:13 -0300

    implementando a listagem de produtos 1/4

---
+++ mini-ecommerce/src/components/produtos/listar-produtos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import placeholder from '../../imagens/286x180.png'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+function ListarProdutos() {
+  return ();
+}
+
+export default ListarProdutos;
