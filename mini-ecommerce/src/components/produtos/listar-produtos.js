a1a25ba53beed107e30c95a3000b1b016242064a
George Salu <george.salu10@gmail.com>
2020-05-04 22:07:07 -0300

    implementando a listagem de produtos 2/4

--- mini-ecommerce/src/components/produtos/listar-produtos.js
+++ mini-ecommerce/src/components/produtos/listar-produtos.js
@@ -4,6 +4,17 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
 function ListarProdutos() {
+
+  const produtos = [
+    { nome: 'aprendendo java', preco: 'R$ 59,00' },
+    { nome: 'javascript em 24', preco: 'R$ 19,00' },
+    { nome: 'react em 7 dias', preco: 'R$ 29,00' },
+    { nome: 'algoritmos e estrutura de dados', preco: 'R$ 25,00' },
+    { nome: 'startup', preco: 'R$ 29,00' },
+    { nome: 'Teste unitarios com jasmine', preco: 'R$ 14,00' },
+    { nome: 'Typescript na pratica', preco: 'R$ 59,00' }
+  ]
+
   return ();
 }
 
