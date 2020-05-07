4c4ece7027defb2bba906f0ab45c91c324eb2986
George Salu <george.salu10@gmail.com>
2020-05-07 11:20:56 -0300

    criando os teste unitarios da listagem de produtos 2/2

--- mini-ecommerce/src/components/produtos/listar-produtos.test.js
+++ mini-ecommerce/src/components/produtos/listar-produtos.test.js
@@ -9,5 +9,15 @@ describe('Teste do componente de listagem de produtos', () => {
     );
     expect(getByTestId('card1')).toHaveTextContent('aprendendo java');
     expect(getByTestId('card2')).toHaveTextContent('javascript em 24');
+  });
+
+  it('deve exibir as descrições dos produtos nos cards', () => {
+    const {getByTestId} = render(
+      <ListarProdutos 
+        adicionarProduto={() => false}
+        exibirMensagem={() => false} />
+    );
+    expect(getByTestId('card1')).toHaveTextContent('Descrição do produto aqui....');
+    expect(getByTestId('card3')).toHaveTextContent('Descrição do produto aqui....');
   })
 })
