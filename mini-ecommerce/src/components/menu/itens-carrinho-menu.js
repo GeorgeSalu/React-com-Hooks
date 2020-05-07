c7aa3610bb54ab4b9a7b74b9e5ce5ed5f96642f4
George Salu <george.salu10@gmail.com>
2020-05-07 17:26:32 -0300

    criando os itens de carrinho no menu 2/3

--- mini-ecommerce/src/components/menu/itens-carrinho-menu.js
+++ mini-ecommerce/src/components/menu/itens-carrinho-menu.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {NavDropdown} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSadTear} from '@fortawesome/free-solid-svg-icons';
 
 function ItensCarrinhoMenu(props) {
-  return ();
+
+  function render() {
+    // carrinho vazio
+    if(props.produtos.length === 0) {
+      return (
+        <NavDropdown.Item href="" data-testid="itens">
+          <FontAwesomeIcon icon={faSadTear} />
+          &nbsp;
+          Carrinho vazio....
+        </NavDropdown.Item>
+      );
+    }
+    //listagem dos produtos
+  }
+  
+  return render();
 }
 
 ItensCarrinhoMenu.propTypes = {
