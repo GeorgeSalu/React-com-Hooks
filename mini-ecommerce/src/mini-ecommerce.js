1ec5c3896dfec7ee5c5eb0d83716c1b52b329690
George Salu <george.salu10@gmail.com>
2020-05-07 15:43:23 -0300

    adicionando propTypes e calculando o preco 2/4

--- mini-ecommerce/src/mini-ecommerce.js
+++ mini-ecommerce/src/mini-ecommerce.js
@@ -32,9 +32,23 @@ function MiniEcommerce() {
     setCarrinho(objCarrinho);
   }
 
+  function handleExibirProdutos() {
+    setExibirCheckout(false);
+    setExibirProdutos(true);
+  }
+
+  function handleExibirCheckout(total) {
+    setExibirCheckout(true);
+    setExibirProdutos(false);
+    setTotal(total);
+  }
+
   return (
     <div>
-      <Menu />
+      <Menu
+        produtos={carrinho.produtos}
+        handleExibirProdutos={handleExibirProdutos}
+        handleExibirCheckout={handleExibirCheckout} />
       <Produtos 
         visivel={exibirProdutos}
         adicionarProduto={adicionarProduto}/>
