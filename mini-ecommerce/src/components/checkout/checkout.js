a1cd800fb2e7154254861ea39b02d33d47087c12
George Salu <george.salu10@gmail.com>
2020-05-08 10:44:17 -0300

    imports para componente de checkout

--- mini-ecommerce/src/components/checkout/checkout.js
+++ mini-ecommerce/src/components/checkout/checkout.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {Form, Row, Col, Button, Jumbotron, Modal} from 'react-bootstrap';
+import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker, {registerLocale} from 'react-datepicker';
+import pt from 'date-fns/locale/pt';
+import PropTypes from 'prop-types';
 
 function Checkout() {
   return (
