3596073857610926679edb1179bd8801d5a5a38b
George Salu <george.salu10@gmail.com>
2020-05-08 11:26:05 -0300

    criando o html do formulario de checkout 2/9

--- mini-ecommerce/src/components/checkout/checkout.js
+++ mini-ecommerce/src/components/checkout/checkout.js
@@ -10,9 +10,32 @@ registerLocale('pt', pt);
 function Checkout(props) {
 
   return (
-    <h1>Checkout</h1>
+    <Jumbotron
+      fluid
+      style={{ margin: '10px' }}>
+        <h3 className="text-center">Finalizar compra</h3>
+
+        <Form noValidate style={{ margin: '10px' }}>
+          <Form.Group as={Row} controlId="email">
+            <Form.Label column sm={3}>
+              Email
+            </Form.Label>
+            <Col sm={9}>
+              <Form.Control
+                type="email"
+                placeholder="Digite o seu email"
+                name="email"
+                data-testid="text-email"/>
+              <Form.Control.Feedback type="invalid">
+                Digite um email valido
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Group>
+        </Form>
+
+    </Jumbotron>
   );
-  
+
 }
 
 export default Checkout;
