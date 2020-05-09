23c1540287e16dc5f4a1fc59ce12b2a26ae71546
George Salu <george.salu10@gmail.com>
2020-05-08 21:40:11 -0300

    listando as cidades no checkout 2/2

--- mini-ecommerce/src/components/checkout/checkout.js
+++ mini-ecommerce/src/components/checkout/checkout.js
@@ -5,6 +5,7 @@ import DatePicker, {registerLocale} from 'react-datepicker';
 import pt from 'date-fns/locale/pt';
 import PropTypes from 'prop-types';
 import ListarEstados from './listar-estados';
+import ListarCidades from './listar-cidades';
 
 registerLocale('pt', pt);
 
@@ -125,7 +126,7 @@ function Checkout(props) {
                 name="cidade"
                 data-testid="cidade">
                   <option value="">Selecion a cidade</option>
-                  {/** listar as cidades aqui */}
+                  <ListarCidades estado={'RJ'} />
               </Form.Control>
               <Form.Control.Feedback type="invalid">
                 Selecione a sua cidade
--- mini-ecommerce/src/components/checkout/listar-cidades.js
+++ mini-ecommerce/src/components/checkout/listar-cidades.js
@@ -7,6 +7,22 @@ function ListarCidades(props) {
   const CIDADES_URL = 'http://localhost:3001/mini-ecommerce/estado/:estado/cidades';
   const [cidades, setCidades] = useState([]);
 
+  useEffect(() => {
+    async function obterCidades() {
+      try {
+        let {data} = await axios.get(CIDADES_URL.replace(':estado',props.estado));
+        setCidades(data);
+      } catch (error) {
+        setCidades([]);
+      }
+    }
+
+    if(props.estado !== '') {
+      obterCidades();
+    }
+
+  }, [props.estado])
+
   return cidades.map(cidade => 
     <option
       key={cidade}
