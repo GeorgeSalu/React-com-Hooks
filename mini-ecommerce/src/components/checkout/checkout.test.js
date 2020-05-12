369f80acebb95b9a6df83614330a59fd1f2633b5
George Salu <george.salu10@gmail.com>
2020-05-11 22:20:58 -0300

    implementando os teste unitarios 1/4

--- mini-ecommerce/src/components/checkout/checkout.test.js
+++ mini-ecommerce/src/components/checkout/checkout.test.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Checkout from './checkout';
+import axiosMock from 'axios'
 
 describe('Teste do componente checkout',() => {
-  it('deve renderizar o componente sem erros', () => {
-    const {getAllByText} = render(<Checkout />);
-    const textos = getAllByText('Finalizar compra');
-    expect(textos[0]).toBeInTheDocument();
-  })
+
+  const dadosFinalizarCompra = {
+    email: 'email@tes.com',
+    nomeCompleto: 'Fulano de tal',
+    cpf: '293.462.345-98',
+    endereco: 'rua dos cadastros, 389',
+    cidade: 'São Paulo',
+    estado: 'SP',
+    cep: '12345-678',
+    termosCondicoes: true,
+    emailPromocional: 'S',
+    dataNascimento: new Date('1998-07-20T23:00:00.000Z'),
+    produtos: '{}',
+    total: 'R$ 10,00'
+  }  
+
 })
